2b0992f22f8e717fb23345ff7fd83a7a91767630
Vladimir Kotal <vlada@kotalovi.cz>
2022-01-27 23:18:45 +0100

    refactor drawing into separate class
    fixes #6

---
+++ display.py
@@ -0,0 +1,234 @@
+"""
+display class
+
+Not dependent on the output type.
+"""
+
+import logging
+from datetime import datetime
+
+import requests
+from PIL import Image, ImageDraw, ImageFont
+
+
+class Display:
+    """
+    Class to wrap fetching and drawing of the metrics.
+    """
+
+    # First define some color constants
+    WHITE = (0xFF, 0xFF, 0xFF)
+    BLACK = (0x00, 0x00, 0x00)
+
+    # Next define some constants to allow easy resizing of shapes and colors
+    BACKGROUND_COLOR = WHITE
+    FOREGROUND_COLOR = WHITE
+    TEXT_COLOR = BLACK
+
+    def __init__(self, url, display_width, display_height, medium_font, large_font):
+        """
+        :param url: URL to retrieve the metrics from
+        :param display_height: display width in pixels
+        :param display_width: display height in pixels
+        :param large_font: path to font used for large letters
+        :param medium_font: path to font used for medium letters
+        """
+        self.url = url
+        self.display_width = display_width
+        self.display_height = display_height
+        self.medium_font_path = medium_font
+        self.large_font_path = large_font
+
+        self.small_font = ImageFont.truetype(self.large_font_path, 12)
+        self.medium_font = ImageFont.truetype(self.medium_font_path, 24)
+        self.large_font = ImageFont.truetype(self.large_font_path, 64)
+
+        self.image = Image.new("RGB", (self.display_width, self.display_height))
+
+        # Get drawing object to draw on image.
+        self.draw = ImageDraw.Draw(self.image)
+
+    def get_metrics(self):
+        """
+        Retrieve metrics from given URL and return them. If a metric cannot be retrieved,
+        None is used instead.
+        :return: tuple of temperature, CO2, atmospheric pressure
+        """
+        temp = None
+        co2 = None
+        pressure = None
+
+        logger = logging.getLogger(__name__)
+
+        try:
+            response = requests.get(self.url)
+        except requests.ConnectionError as req_exc:
+            logger.error(f"cannot get data from {self.url}: {req_exc}")
+            return temp, co2, pressure
+
+        if response.status_code != 200:
+            logger.error(
+                f"cannot retrieve metrics from {self.url}: {response.status_code}"
+            )
+        else:
+            lines = response.text.split("\n")
+            for line in lines:
+                if line.startswith("weather_temp_terasa"):
+                    _, temp = line.split()
+                    continue
+
+                if line.startswith("co2_ppm"):
+                    _, co2 = line.split()
+                    continue
+
+                if line.startswith("pressure_sea_level_hpa"):
+                    _, pressure = line.split()
+                    continue
+
+        return temp, co2, pressure
+
+    def draw_image(self):
+        """
+        Refresh the display with weather metrics retrieved from the URL
+        :return PIL image instance
+        """
+
+        temp, co2, pressure = self.get_metrics()
+
+        # Draw a filled box as the background
+        self.draw.rectangle(
+            (0, 0, self.display_width - 1, self.display_height - 1),
+            fill=Display.BACKGROUND_COLOR,
+        )
+
+        self.draw_date_time()
+
+        text_height = self.draw_outside_temperature(temp)
+        current_height = self.draw_co2(co2, text_height)
+        self.draw_pressure(current_height, pressure)
+
+        return self.image
+
+    def draw_pressure(self, current_height, pressure):
+        """
+        :param current_height:
+        :param pressure:
+        :return:
+        """
+        logger = logging.getLogger(__name__)
+
+        # Display atmospheric pressure level.
+        if pressure:
+            pressure = int(float(pressure))
+            text = f"Pressure: {pressure} hPa"
+        else:
+            text = "Pressure: N/A"
+        logger.debug(text)
+        coordinates = (0, current_height)  # use previous text height
+        logger.debug(f"coordinates = {coordinates}")
+        self.draw.text(
+            coordinates,
+            text,
+            font=self.medium_font,
+            fill=Display.TEXT_COLOR,
+        )
+
+    def draw_co2(self, co2, text_height):
+        """
+        :param co2:
+        :param text_height:
+        :return: current text height
+        """
+        logger = logging.getLogger(__name__)
+
+        # Display CO2 level.
+        co_text = "CO"
+        if co2:
+            co2 = int(float(co2))
+            text = f"{co_text} : {co2} ppm"
+        else:
+            text = f"{co_text} : N/A"
+        coordinates = (0, text_height + 10)  # use previous text height
+        current_height = text_height + 10
+        (_, text_height) = self.medium_font.getsize(text)
+        current_height = current_height + text_height
+        logger.debug(f"'{text}' coordinates = {coordinates}")
+        self.draw.text(
+            coordinates,
+            text,
+            font=self.medium_font,
+            fill=Display.TEXT_COLOR,
+        )
+
+        (co_width, _) = self.medium_font.getsize(co_text)
+        coordinates = (co_width, current_height - 10)
+        logger.debug(f"'2' coordinates = {coordinates}")
+        self.draw.text(
+            coordinates,
+            "2",
+            font=self.small_font,
+            fill=Display.TEXT_COLOR,
+        )
+
+        return current_height
+
+    def draw_outside_temperature(self, temp):
+        """
+        :param draw:
+        :param temp: temperature value
+        :return: current text height
+        """
+        logger = logging.getLogger(__name__)
+
+        # Display outside temperature.
+        if temp:
+            outside_temp = int(float(temp))
+            text = f"{outside_temp}°C"
+        else:
+            text = "N/A"
+        logger.debug(text)
+        (text_width, text_height) = self.large_font.getsize(text)
+        logger.debug(f"text width={text_width}, height={text_height}")
+        logger.debug(
+            f"display width={self.display_width}, height={self.display_height}"
+        )
+        coordinates = (0, 0)
+        logger.debug(f"coordinates = {coordinates}")
+        self.draw.text(
+            coordinates,
+            text,
+            font=self.large_font,
+            fill=Display.TEXT_COLOR,
+        )
+        return text_height
+
+    def draw_date_time(self):
+        """
+        Draw date and time in the top right corner.
+        """
+        logger = logging.getLogger(__name__)
+
+        # Display time.
+        now = datetime.now()
+        text = now.strftime(f"{now.hour}:%M")
+        logger.debug(text)
+        (text_width, text_height) = self.medium_font.getsize(text)
+        coordinates = (self.display_width - text_width - 10, 0)
+        logger.debug(f"coordinates = {coordinates}")
+        self.draw.text(
+            coordinates,
+            text,
+            font=self.medium_font,
+            fill=Display.TEXT_COLOR,
+        )
+        # Display date underneath the time.
+        text = now.strftime(f"{now.day}.{now.month}.")
+        logger.debug(text)
+        coordinates = (self.display_width - text_width - 10, text_height + 5)
+        logger.debug(f"coordinates = {coordinates}")
+        self.draw.text(
+            coordinates,
+            text,
+            font=self.medium_font,
+            fill=Display.TEXT_COLOR,
+        )
--- report.py
+++ report.py
@@ -8,7 +8,6 @@ import argparse
 import logging
 import sys
 import time
-from datetime import datetime
 
 try:
     import board
@@ -16,58 +15,10 @@ try:
     import digitalio
 except NotImplementedError as exc:
     print(f"Will only support running with -o: {exc}")
-import requests
 from adafruit_epd.ssd1680 import Adafruit_SSD1680
-from PIL import Image, ImageDraw, ImageFont
 
+from display import Display
 from logutil import LogLevelAction
-
-# First define some color constants
-WHITE = (0xFF, 0xFF, 0xFF)
-BLACK = (0x00, 0x00, 0x00)
-
-# Next define some constants to allow easy resizing of shapes and colors
-BACKGROUND_COLOR = WHITE
-FOREGROUND_COLOR = WHITE
-TEXT_COLOR = BLACK
-
-
-def get_metrics(url):
-    """
-    Retrieve metrics from given URL and return them. If a metric cannot be retrieved,
-    None is used instead.
-    :return: tuple of temperature, CO2, atmospheric pressure
-    """
-    temp = None
-    co2 = None
-    pressure = None
-
-    logger = logging.getLogger(__name__)
-
-    try:
-        response = requests.get(url)
-    except requests.ConnectionError as req_exc:
-        logger.error(f"cannot get data from {url}: {req_exc}")
-        return temp, co2, pressure
-
-    if response.status_code != 200:
-        logger.error(f"cannot retrieve metrics from {url}: {response.status_code}")
-    else:
-        lines = response.text.split("\n")
-        for line in lines:
-            if line.startswith("weather_temp_terasa"):
-                _, temp = line.split()
-                continue
-
-            if line.startswith("co2_ppm"):
-                _, co2 = line.split()
-                continue
-
-            if line.startswith("pressure_sea_level_hpa"):
-                _, pressure = line.split()
-                continue
-
-    return temp, co2, pressure
 
 
 def get_e_ink_display(height, width):
@@ -96,179 +47,6 @@ def get_e_ink_display(height, width):
     display.rotation = 1
 
     return display
-
-
-def draw_image(url, display_width, display_height, medium_font_path, large_font_path):
-    """
-    Refresh the display with weather metrics retrieved from the URL
-    :param large_font_path: path to font used for large letters
-    :param medium_font_path: path to font used for medium letters
-    :param display_height: display width in pixels
-    :param display_width: display height in pixels
-    :param url: URL to retrieve the metrics from
-    :return PIL image instance
-    """
-
-    small_font = ImageFont.truetype(large_font_path, 12)
-    medium_font = ImageFont.truetype(medium_font_path, 24)
-    large_font = ImageFont.truetype(large_font_path, 64)
-
-    temp, co2, pressure = get_metrics(url)
-
-    image = Image.new("RGB", (display_width, display_height))
-
-    # Get drawing object to draw on image.
-    draw = ImageDraw.Draw(image)
-
-    # Draw a filled box as the background
-    draw.rectangle((0, 0, display_width - 1, display_height - 1), fill=BACKGROUND_COLOR)
-
-    draw_date_time(display_width, draw, medium_font)
-
-    text_height = draw_outside_temperature(
-        draw, temp, display_height, display_width, large_font
-    )
-    current_height = draw_co2(draw, co2, text_height, medium_font, small_font)
-    draw_pressure(current_height, draw, medium_font, pressure)
-
-    return image
-
-
-def draw_pressure(current_height, draw, medium_font, pressure):
-    """
-    :param current_height:
-    :param draw:
-    :param medium_font:
-    :param pressure:
-    :return:
-    """
-    logger = logging.getLogger(__name__)
-
-    # Display atmospheric pressure level.
-    if pressure:
-        pressure = int(float(pressure))
-        text = f"Pressure: {pressure} hPa"
-    else:
-        text = "Pressure: N/A"
-    logger.debug(text)
-    coordinates = (0, current_height)  # use previous text height
-    logger.debug(f"coordinates = {coordinates}")
-    draw.text(
-        coordinates,
-        text,
-        font=medium_font,
-        fill=TEXT_COLOR,
-    )
-
-
-def draw_co2(draw, co2, text_height, medium_font, small_font):
-    """
-    :param draw:
-    :param co2:
-    :param text_height:
-    :param medium_font:
-    :return: current text height
-    """
-    logger = logging.getLogger(__name__)
-
-    # Display CO2 level.
-    co_text = "CO"
-    if co2:
-        co2 = int(float(co2))
-        text = f"{co_text} : {co2} ppm"
-    else:
-        text = f"{co_text} : N/A"
-    coordinates = (0, text_height + 10)  # use previous text height
-    current_height = text_height + 10
-    (_, text_height) = medium_font.getsize(text)
-    current_height = current_height + text_height
-    logger.debug(f"'{text}' coordinates = {coordinates}")
-    draw.text(
-        coordinates,
-        text,
-        font=medium_font,
-        fill=TEXT_COLOR,
-    )
-
-    (co_width, _) = medium_font.getsize(co_text)
-    coordinates = (co_width, current_height - 10)
-    logger.debug(f"'2' coordinates = {coordinates}")
-    draw.text(
-        coordinates,
-        "2",
-        font=small_font,
-        fill=TEXT_COLOR,
-    )
-
-    return current_height
-
-
-def draw_outside_temperature(draw, temp, display_height, display_width, large_font):
-    """
-    :param draw:
-    :param temp:
-    :param display_height:
-    :param display_width:
-    :param large_font:
-    :return: current text height
-    """
-    logger = logging.getLogger(__name__)
-
-    # Display outside temperature.
-    if temp:
-        outside_temp = int(float(temp))
-        text = f"{outside_temp}°C"
-    else:
-        text = "N/A"
-    logger.debug(text)
-    (text_width, text_height) = large_font.getsize(text)
-    logger.debug(f"text width={text_width}, height={text_height}")
-    logger.debug(f"display width={display_width}, height={display_height}")
-    coordinates = (0, 0)
-    logger.debug(f"coordinates = {coordinates}")
-    draw.text(
-        coordinates,
-        text,
-        font=large_font,
-        fill=TEXT_COLOR,
-    )
-    return text_height
-
-
-def draw_date_time(display_width, draw, medium_font):
-    """
-    Draw date and time in the right top corner.
-    :param display_width:
-    :param draw:
-    :param medium_font:
-    :return:
-    """
-    logger = logging.getLogger(__name__)
-
-    # Display time.
-    now = datetime.now()
-    text = now.strftime(f"{now.hour}:%M")
-    logger.debug(text)
-    (text_width, text_height) = medium_font.getsize(text)
-    coordinates = (display_width - text_width - 10, 0)
-    logger.debug(f"coordinates = {coordinates}")
-    draw.text(
-        coordinates,
-        text,
-        font=medium_font,
-        fill=TEXT_COLOR,
-    )
-    # Display date underneath the time.
-    text = now.strftime(f"{now.day}.{now.month}.")
-    logger.debug(text)
-    coordinates = (display_width - text_width - 10, text_height + 5)
-    logger.debug(f"coordinates = {coordinates}")
-    draw.text(
-        coordinates,
-        text,
-        font=medium_font,
-        fill=TEXT_COLOR,
-    )
 
 
 def update_e_ink_display(display, image):
@@ -348,19 +126,21 @@ def main():
     display_height = 122
     display_width = 250
 
+    display = Display(
+        args.url, display_width, display_height, args.medium_font, args.large_font
+    )
     if args.output:
-        image = draw_image(
-            args.url, display_width, display_height, args.medium_font, args.large_font
-        )
+        image = display.draw_image()
         image.save(args.output)
         return
 
-    display = get_e_ink_display(display_height, display_width)
+    e_display = get_e_ink_display(display_height, display_width)
+    display = Display(
+        args.url, e_display.width, e_display.height, args.medium_font, args.large_font
+    )
     while True:
-        image = draw_image(
-            args.url, display.width, display.height, args.medium_font, args.large_font
-        )
-        update_e_ink_display(display, image)
+        image = display.draw_image()
+        update_e_ink_display(e_display, image)
         time.sleep(args.timeout)
 
 
